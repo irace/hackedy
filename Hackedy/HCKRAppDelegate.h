262f9a9d1d7e08ccc80af0e8d7d703f103f76337
Bryan Irace <bryan.irace@gmail.com>
2014-01-08 23:37:30 -0500

    Initial Commit

---
+++ Hackedy/HCKRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HCKRAppDelegate.h
+//  Hackedy
+//
+//  Created by Bryan Irace on 1/8/14.
+//  Copyright (c) 2014 Bryan Irace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HCKRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
